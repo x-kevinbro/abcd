c3286f299bbd750415bef341f37f0561b6b1e710
x-kevinbro <damiruxox@outlook.com>
2025-09-04 00:05:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
+SESSION_ID: process.env.SESSION_ID === undefined ? '7gAREQ6b#J-aL2v2TXZ79Rr1KRv6toyU_WlBerM9CRfI1krDrR3I' : process.env.SESSION_ID,
+PORT: process.env.PORT === undefined ? "8080" : process.env.PORT,
+SESSION_NAME: process.env.PORT === undefined ? "kevin" : process.env.SESSION_NAME,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://xkevin:Dam@123%@188.166.191.216:5432/my_new_database' : process.env.POSTGRESQL_URL,
 };
